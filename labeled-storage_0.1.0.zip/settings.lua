5467131bb73341ef2f5583aebdebb0ba999a73a2
Cricket Beautiful Onebit <90658143+CricketBr@users.noreply.github.com>
2024-07-24 19:10:50 -0400

    Delete labeled-storage_0.1.0.zip directory
    Signed-off-by: Cricket Beautiful Onebit <90658143+CricketBr@users.noreply.github.com>

--- labeled-storage_0.1.0.zip/settings.lua
+++
@@ -1,7 +0,0 @@
-data:extend(
-    {
-        { name = 'cb-ls-logistic-labels', type = 'bool-setting', setting_type = 'startup', default_value = true, order = '01', localised_description = 'logistic chests get letters, not just colours' },
-        { name = 'cb-ls-many-containers', type = 'bool-setting', setting_type = 'startup', default_value = true, order = '02', localised_description = 'labeled storage containers, for easier troubleshooting' },
-        { name = 'cb-ls-tiny-chests',     type = 'bool-setting', setting_type = 'startup', default_value = true, order = '04', localised_description = '1, 2 and 3 stack chests, 1x1 tiles, storage are pre-filtered' },
-    }
-)
